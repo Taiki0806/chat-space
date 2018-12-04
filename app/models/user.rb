cb2199d077eddb51a427e05fbed9a15ab474131d
Taiki0806 <ty4086@iCloud.com>
2018-12-04 12:21:04 +0900

    add-group-function
    グループ作成機能の実装。
    グループ編集機能の実装。

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :members
-  has_many :users, through: :members
+  has_many :groups, through: :members
 end
