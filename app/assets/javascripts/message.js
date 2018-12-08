c8e255f41b1b949b947b800f574268ce3365d79c
Taiki0806 <ty4086@iCloud.com>
2018-12-08 19:39:24 +0900

    Edit-message.js
    指摘された箇所の修正を行いました。
    If文を三項演算子に編集しました。
    $(".chat-main__footer__form__send-button").prop("disabled", false);
    の記述をsubmitイベントの際に行うように
    編集しました。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,10 +1,11 @@
 $(function() {
   function buildHTML(message) {
     var imageUrl = "";
-    if (message.image.url) {
-      var imageUrl = `<div class="chat-main__content__message-block__picture"></div>
-                        <img src="${message.image.url}", class="chat-main__content__message-block__image">`
-    }
+    var imageUrl = (message.image.url)?
+           `<div class="chat-main__content__message-block__picture"></div>
+              <img src="${message.image.url}", class="chat-main__content__message-block__image">`
+              : "";
+
     var html = `<div class="chat-main__content__message-block">
                   <p class="chat-main__content__message-block__name">
                     ${message.user_name}
@@ -19,9 +20,6 @@ $(function() {
                 </div>`
                   return html
   }
-  $("#new_message").on('click', function(e){
-    $(".chat-main__footer__form__send-button").prop("disabled", false);
-  });
 
   $(document).on('submit', "#new_message", function(e){
     e.preventDefault();
@@ -40,6 +38,7 @@ $(function() {
       $(".chat-main__content").append(html);
       $(".chat-main__footer__form__message-form").val('');
       $(".chat-main__footer__form__label__file").val('');
+      $(".chat-main__footer__form__send-button").prop("disabled", false);
       $("html,body").animate({scrollTop: $("html,body")[0].scrollHeight}, 'fast');
     })
     .fail(function(){
