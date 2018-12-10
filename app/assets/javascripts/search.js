033b3680a0678e1b70e2fb996fff75843628e3da
Taiki0806 <ty4086@iCloud.com>
2018-12-10 13:52:36 +0900

    Edit-search.js-users_controller.rb
    指摘箇所の修正をしました。
    キャメルケースへの統一。
    format.htmlの削除。

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,22 +1,22 @@
 $(function() {
 
-  var search_list = $('#user-search-result');
+  var searchList = $('#user-search-result');
 
-  var member_list = $('#chat-group-users');
+  var memberList = $('#chat-group-users');
 
   function appendUser(user) {
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user.name}</p>
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
                 </div>`
-    search_list.append(html);
+    searchList.append(html);
   }
 
   function appendNoUser(noUserMessage) {
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${noUserMessage}</p>
                 </div>`
-    search_list.append(html);
+    searchList.append(html);
   }
 
   function appendMember(userId, userName) {
@@ -25,7 +25,7 @@ $(function() {
                   <p class='chat-group-user__name'>${userName}</p>
                   <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
                 </div>`
-    member_list.append(html);
+    memberList.append(html);
   }
 
   $('#user-search-field').on('keyup', function() {
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@ class UsersController < ApplicationController
   def index
     @users = User.where('name LIKE(?)', "%#{params[:name]}%")
     respond_to do |format|
-      format.html
       format.json
     end
   end
